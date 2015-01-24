54a62ad1e37c5b6da7c7adcd55894c3518f6f2e2
Ramakrishna Makkena <ramakrishnamakkena@ramakrishnasmbp.gateway.pace.com>
2015-01-24 13:57:36 -0500

    added new test class

---
+++ Age of Laika/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Age of Laika
+//
+//  Created by Ramakrishna Makkena on 1/24/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Age of Laika/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Age of Laika
+//
+//  Created by Ramakrishna Makkena on 1/24/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
